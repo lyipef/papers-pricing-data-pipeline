19f879d0f8029d4a2d6fdc4f6fbd4fa589cbbe06
lyipef <josefilipe602@gmail.com>
2025-04-27 22:22:45 -0300

    usd_cny dag working

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,48 @@
-FROM astrocrpublic.azurecr.io/runtime:3.0-1
+FROM quay.io/astronomer/astro-runtime:11.6.0
 
-# install pandas into a virtual environment
-RUN python -m venv pandas_venv && source pandas_venv/bin/activate && \
-    pip install --no-cache-dir pandas && deactivate
+# Instala o pandas e outras dependências em um ambiente virtual
+RUN python -m venv pandas_venv && \
+    source pandas_venv/bin/activate && \
+    pip install --no-cache-dir google-cloud-bigquery-storage && \
+    pip install --no-cache-dir selenium && \
+    pip install --no-cache-dir beautifulsoup4 && \
+    pip install --no-cache-dir pandas && \
+    deactivate
+
+# Instala o Google Chrome e o ChromeDriver
+USER root
+RUN apt-get update -y && apt-get install -y wget xvfb unzip jq
+
+# Install Google Chrome dependencies
+RUN apt-get install -y libxss1 libappindicator1 libgconf-2-4 \
+    fonts-liberation libasound2 libnspr4 libnss3 libx11-xcb1 libxtst6 lsb-release xdg-utils \
+    libgbm1 libnss3 libatk-bridge2.0-0 libgtk-3-0 libx11-xcb1 libxcb-dri3-0
+
+
+# Fetch the latest version numbers and URLs for Chrome and ChromeDriver
+RUN curl -s https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json > /tmp/versions.json
+
+RUN CHROME_URL=$(jq -r '.channels.Stable.downloads.chrome[] | select(.platform=="linux64") | .url' /tmp/versions.json) && \
+    wget -q --continue -O /tmp/chrome-linux64.zip $CHROME_URL && \
+    unzip /tmp/chrome-linux64.zip -d /opt/chrome
+
+RUN chmod +x /opt/chrome/chrome-linux64/chrome
+
+
+RUN CHROMEDRIVER_URL=$(jq -r '.channels.Stable.downloads.chromedriver[] | select(.platform=="linux64") | .url' /tmp/versions.json) && \
+    wget -q --continue -O /tmp/chromedriver-linux64.zip $CHROMEDRIVER_URL && \
+    unzip /tmp/chromedriver-linux64.zip -d /opt/chromedriver && \
+    chmod +x /opt/chromedriver/chromedriver-linux64/chromedriver
+
+# Set up Chromedriver Environment variables
+ENV CHROMEDRIVER_DIR /opt/chromedriver
+ENV PATH $CHROMEDRIVER_DIR:$PATH
+
+# Clean upa
+RUN rm /tmp/chrome-linux64.zip /tmp/chromedriver-linux64.zip /tmp/versions.json
+
+# python dependencies
+RUN pip install selenium
+
+# Retorna para o usuário 'astro' do container
+USER astro
---
+++ dags/teste.py
@@ -0,0 +1,105 @@
+from airflow.decorators import dag, task
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from airflow.models.baseoperator import chain
+
+from datetime import datetime
+import pandas as pd
+import json
+
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
+
+
+@dag(
+    start_date=datetime(2025, 4, 25),
+    schedule=None,
+    catchup=False,
+    tags=["papers_pricing"],
+)
+def papers():
+    bucket_name = "data-engineer-test-suzano"
+    
+
+    @task
+    def fetch_and_upload():
+        from selenium import webdriver
+        from selenium.webdriver.chrome.options import Options
+        from bs4 import BeautifulSoup
+        import time
+
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--headless=new")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--lang=pt-BR")
+        options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_argument('--disable-blink-features=AutomationControlled')
+        options.add_experimental_option("useAutomationExtension", False)
+
+
+        driver = webdriver.Chrome(options=options)
+
+        time.sleep(5)
+        driver.get("https://api.investing.com/api/financialdata/2111/historical/chart/?interval=P1M&period=MAX&pointscount=160")
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+    
+        tag = soup.find("pre")
+        if not tag:
+            raise ValueError("Conteúdo esperado não encontrado")
+
+        data = json.loads(tag.text).get("data", [])
+        df = pd.DataFrame(data, columns=["timestamp", "close", "open", "high", "low", "volume", "unknown"])
+        df["date"] = pd.to_datetime(df["timestamp"], unit="ms")
+        df = df[df["date"] >= "1991-01-01"]
+        df.drop(columns=["timestamp", "unknown"], inplace=True)
+        df = df[["date", "close", "open", "high", "low", "volume"]]
+        df.to_parquet("/usr/local/airflow/include/datasets/usd_cny.parquet", index=False)
+
+        driver.quit()
+
+    
+    upload_usd_cny_parquet_to_gcs = LocalFilesystemToGCSOperator(
+        task_id="upload_usd_cny_parquet_to_gcs",
+        src="/usr/local/airflow/include/datasets/usd_cny.parquet",
+        dst="raw/usd_cny.parquet",
+        bucket=bucket_name,
+        gcp_conn_id="gcp",
+        mime_type="application/octet-stream",
+    )
+
+
+    create_papers_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id="create_papers_dataset",
+        dataset_id="papers",
+        gcp_conn_id="gcp",
+    )
+
+    papers_gcs_to_raw = aql.load_file(
+        task_id="papers_gcs_to_raw",
+        input_file=File(
+            f"gs://{bucket_name}/raw/usd_cny.parquet",
+            conn_id="gcp",
+            filetype=FileType.PARQUET,
+        ),
+        output_table=Table(
+            name="usd_cny_monthly",
+            conn_id="gcp",
+            metadata=Metadata(schema="papers")
+        ),
+        use_native_support=True,
+    )
+
+    chain(
+        fetch_and_upload(),
+        upload_usd_cny_parquet_to_gcs,
+        create_papers_dataset,
+        papers_gcs_to_raw
+    )
+
+papers()
